b9d70df2faf5f71f8726a051ead111c90b36cf5e
Kyle Talmage <ktalmage85@gmail.com>
2020-02-04 19:02:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,12 +27,12 @@ end
 def update_counting_hash(hash, key)
   
     if hash[key]
-      hash[key] +=1
-    else
-      hash[key] = 1
-    end
-    puts hash
+    hash[key] += 1
+    return hash
+  else hash[key] = 1
+    return hash
   end
+end
 
 hash = {:apples => 3}
 key = :oranges
